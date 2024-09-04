64a6bfc2f1783e21cc84e6174966a185241e9b7e
daggerallocca <alloccaa6852@student.faytechcc.edu>
2024-09-04 17:58:32 +0000

    changed file

---
+++ P1LAB1.py
@@ -0,0 +1,11 @@
+# Dagger Allocca
+# 09/04/2024
+# P1LAB1
+# Input and Output
+
+# The input function always saves the data as a string, by default.
+
+firstname = input("Enter your first name: ")
+
+lastname = input("Enter your last name: ")
+print("Hello",firstname,lastname+"!","Welcome to CTI-110")
