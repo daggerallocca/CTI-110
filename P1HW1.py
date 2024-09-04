62faa2392df0224cc1eff68cbaad25b557d5b8fa
daggerallocca <alloccaa6852@student.faytechcc.edu>
2024-09-04 18:32:50 +0000

    added addition and subtraction

---
+++ P1HW1.py
@@ -0,0 +1,35 @@
+# Dagger Allocca
+# 09/04/2024
+# P1HW1
+# Input and output with integers
+
+print("-----Calculating Exponents-----")
+
+print("")
+print("")
+
+basevalue = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+
+print("")
+print("")
+
+print(basevalue,"raised to the power of",exponent,"is",basevalue**exponent,"!!")
+
+print("")
+print("")
+
+print("-----Addition and Subtraction-----")
+
+print("")
+print("")
+
+int1 = int(input("Enter a starting integer: "))
+int2 = int(input("Enter an integer to add: "))
+int3 = int(input("Enter an integer to subtract: "))
+
+print("")
+print("")
+
+answer = int1+int2-int3
+print(int1,"+",int2,"-",int3,"is equal to",answer)
