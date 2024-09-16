04ff050bc41bbc239facda84deca3705f630f063
daggerallocca <alloccaa6852@student.faytechcc.edu>
2024-09-16 18:26:19 +0000

    eee

--- P2HW1.py
+++ P2HW1.py
@@ -7,28 +7,26 @@ print('This program calculates and displays travel expenses')
 
 print('')
 
-budget = (input('Enter Budget: '))
+budget = float(input('Enter Budget: '))
 destination = input('Enter your travel destination: ')
-gas = (input('How much do you think you will spend on gas? '))
-accomodation = (input('Approximately, how much do you think you will need for accomodation/hotel? '))
-food = (input('Last, how much do you need for food? '))
+gas = float(input('How much do you think you will spend on gas? '))
+accomodation = float(input('Approximately, how much do you think you will need for accomodation/hotel? '))
+food = float(input('Last, how much do you need for food? '))
 
 print('')
 
 print('-----Travel Expenses-----')
 print('Location:',destination)
-print("f'{budget:.2f}'")
+print(f"${budget:.2f}")
 
 print('')
 
-print('Fuel:',"f'${gas:.2f}'")
-print('Accomodation:',"f'${accomodation:.2f}'")
-print('Food:',"f'${food:.2f}'")
+print('Fuel:',f"${gas:.2f}")
+print('Accomodation:',f"${accomodation:.2f}")
+print('Food:',f"${food:.2f}")
 
 print('')
 
 total = gas + accomodation + food
 balance = budget - total
-print('Remaining Balance:',balance)
-
-
+print('Remaining Balance:',f"${balance:.2f}")
